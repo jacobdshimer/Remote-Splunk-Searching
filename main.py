06a46bb9a0650aa6fe4d4d3828c8d774536b42bd
jacobdshimer <31100766+jacobdshimer@users.noreply.github.com>
2018-07-25 15:13:07 -0400

    Removed static information

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import authenticate as auth
 import search
 
-hostname = "ds37812-14.class.splunk.com"
-sessionKey = auth.getLogon(hostname,"admin","splunk","8089")
-srch = search.sendSearch(sessionKey,hostname,"8089","index=_internal | top stats host")
+hostname = ""
+username = ""
+password = ""
+splunkdPort = ""
+searchString = ""
+sessionKey = auth.getLogon(hostname,username,password,splunkdPort)
+srch = search.sendSearch(sessionKey,hostname,splunkdPort,searchString)
